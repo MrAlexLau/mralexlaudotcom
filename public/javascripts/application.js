57b0cdf6470de52750e51c9b948af309e4ff0629
mralexlau <laux0083@gmail.com>
2014-09-20 10:41:11 -0500

    Centered circles

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,6 +11,20 @@ $('.skills a').on('click', function() {
   setTimeout(showDetails('skills'), 1500);
 });
 
+$('.planet').on('click', function(e) {
+  var $target = $(e.target).parents('.planet');
+
+  $target.addClass('zoom-effect');
+
+  setTimeout(showDetails($target.data('section')), 1500);
+});
+
+$('.close').on('click', function(e){
+  $('.planet').removeClass('zoom-effect');
+  $('.menu').fadeIn(500);
+  $(e.target).parent().fadeOut(500);
+});
+
 $('.orbit').on('mouseover', function() {
   $('.orbit, .inner').addClass('paused');
 });
